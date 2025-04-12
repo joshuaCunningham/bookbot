6ade78d1ab7ba223ad9f1e09bac6e9db458b58b4
joshuaCunningham <josh.cham01@gmail.com>
2025-04-11 20:33:28 -0700

    final bookbot changes

--- main.py
+++ main.py
@@ -1,31 +1,39 @@
 from stats import word_count
 from stats import char_count
 from stats import sorted_char_count
+import sys
 
 def get_book_text(path_to_file):
     with open(path_to_file) as file:
         book_text = file.read()
     return book_text
 
-char_count = char_count(get_book_text('books/frankenstein.txt'))
+
 
 def main():
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
-    print("----------- Word Count ----------")
-    print(f"Found {word_count(get_book_text('books/frankenstein.txt'))} total words")
-    print("--------- Character Count -------")
+    # char_count = char_count(get_book_text(sys.argv[1]))
 
-    for char_dict in sorted_char_count(char_count):
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {sys.argv[1]}...")
+        
+        book_text = get_book_text(sys.argv[1])
+        print("----------- Word Count ----------")
+        print(f"Found {word_count(get_book_text(sys.argv[1]))} total words")
+        print("--------- Character Count -------")
 
-        char = char_dict["char"]
-        count = char_dict["count"]
+        char_count_result = char_count(get_book_text(sys.argv[1]))
+        for char_dict in sorted_char_count(char_count_result):
 
-        if char.isalpha():
-            print(f"{char}: {count}")
+            char = char_dict["char"]
+            count = char_dict["count"]
 
-
-    print("============= END ===============")
+            if char.isalpha():
+                print(f"{char}: {count}")
+        print("============= END ===============")
 
 
 main()
